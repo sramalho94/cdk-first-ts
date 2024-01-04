a53538c59c97f93726d990cb6d24576ab7ca160e
Stephan D Ramalho <sramalho@fordham.edu>
2024-01-04 13:57:48 -0500

    Initial commit

--- lib/cdk-first-ts-stack.ts
+++ lib/cdk-first-ts-stack.ts
@@ -1,16 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb'
+import { Runtime, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkFirstTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkFirstTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    super(scope, id, props)
   }
 }
