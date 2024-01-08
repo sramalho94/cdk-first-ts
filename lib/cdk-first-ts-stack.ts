8d872d9e26d7991574e2c304d09104220ff59d78
Stephan D Ramalho <sramalho@fordham.edu>
2024-01-08 18:23:14 -0500

    Configure cdk app with lambda functions, dynamo table, functionUrl, and function output

--- lib/cdk-first-ts-stack.ts
+++ lib/cdk-first-ts-stack.ts
@@ -4,6 +4,7 @@ import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib'
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb'
 import { Runtime, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda'
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as path from 'path'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkFirstTsStack extends cdk.Stack {
@@ -13,6 +14,30 @@ export class CdkFirstTsStack extends cdk.Stack {
     // Dynamodb table definition
     const table = new Table(this, 'Request', {
       partitionKey: { name: 'username', type: AttributeType.STRING }
+    })
+
+    // lambda function
+    const dynamoLambda = new NodejsFunction(this, 'DynamoLambdaHandler', {
+      runtime: Runtime.NODEJS_18_X,
+      entry: path.join(__dirname, `/../functions/function.ts`),
+      handler: 'handler',
+      environment: {
+        REQUEST_TABLE_NAME: table.tableName
+      }
+    })
+
+    // permissions to lambda to dynamo table
+    table.grantReadWriteData(dynamoLambda)
+
+    const myFunctionUrl = dynamoLambda.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*']
+      }
+    })
+
+    new CfnOutput(this, 'FunctionUrl', {
+      value: myFunctionUrl.url
     })
   }
 }
